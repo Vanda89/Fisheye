721a05fb25e2ff63cf43aec99e8f2adcaba2414a
Vanda89 <sandrinealcazar@gmail.com>
2024-01-11 15:54:31 +0100

    chore : revision after oral presentation

--- scripts/utils/dropdown.js
+++ scripts/utils/dropdown.js
@@ -18,13 +18,19 @@ const dropdown = {
     $sortingDropdown.setAttribute('aria-label', 'Menu déroulant fermé. Cliquez pour trier les médias par plusieurs options.')
     $sortingDropdown.setAttribute('tabindex', '0')
 
-    const $chevronDown = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    const $chevronDown = document.createElement('button')
     $chevronDown.classList.add('chevron', 'chevron-down')
-    $chevronDown.setAttribute('viewBox', '0 0 512 512')
-    $chevronDown.setAttribute('fill', 'white')
-    $chevronDown.innerHTML = '<path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/>'
-    $chevronDown.setAttribute('width', '24')
-    $chevronDown.setAttribute('height', '24')
+    $chevronDown.setAttribute('type', 'button')
+    $chevronDown.setAttribute('aria-label', 'Menu déroulant fermé. Cliquez pour trier les médias par plusieurs options.')
+
+    const $chevronDownIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    $chevronDownIcon.classList.add('chevron-down-icon')
+    $chevronDownIcon.setAttribute('aria-hidden', 'true')
+    $chevronDownIcon.setAttribute('viewBox', '0 0 512 512')
+    $chevronDownIcon.setAttribute('fill', 'white')
+    $chevronDownIcon.innerHTML = '<path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/>'
+    $chevronDownIcon.setAttribute('width', '24')
+    $chevronDownIcon.setAttribute('height', '24')
 
     const $sortMenu = document.createElement('div')
     $sortMenu.id = 'listbox'
@@ -53,22 +59,31 @@ const dropdown = {
     $option3.setAttribute('role', 'option')
     $option3.textContent = 'Titre'
 
-    const $chevronUp = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    const $chevronUp = document.createElement('button')
     $chevronUp.classList.add('chevron', 'chevron-up')
-    $chevronUp.setAttribute('viewBox', '0 0 512 512')
-    $chevronUp.setAttribute('fill', 'white')
-    $chevronUp.innerHTML = '<path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"/>'
-    $chevronUp.setAttribute('width', '24')
-    $chevronUp.setAttribute('height', '24')
+    $chevronUp.setAttribute('type', 'button')
+    $chevronUp.setAttribute('aria-label', 'Menu déroulant ouvert. Cliquez pour fermer le menu déroulant.')
 
-    $sortMenu.appendChild($option1)
+    const $chevronUpIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    $chevronUpIcon.classList.add('chevron-up-icon')
+    $chevronUpIcon.setAttribute('viewBox', '0 0 512 512')
+    $chevronUpIcon.setAttribute('fill', 'white')
+    $chevronUpIcon.innerHTML = '<path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"/>'
+    $chevronUpIcon.setAttribute('width', '24')
+    $chevronUpIcon.setAttribute('height', '24')
+
+    $chevronUp.appendChild($chevronUpIcon)
+
     $sortMenu.appendChild($chevronUp)
+    $sortMenu.appendChild($option1)
     $sortMenu.appendChild($option2)
     $sortMenu.appendChild($option3)
 
-    $dropdown.appendChild($chevronDown)
+    $chevronDown.appendChild($chevronDownIcon)
+
     $dropdown.appendChild($sortBy)
     $dropdown.appendChild($sortingDropdown)
+    $dropdown.appendChild($chevronDown)
     $dropdown.appendChild($sortMenu)
 
     return $dropdown
@@ -76,12 +91,19 @@ const dropdown = {
 
   initializeDropdown () {
     const $sortingDropdown = document.querySelector('.sortingDropdown')
+    const $chevronDown = document.querySelector('.chevron-down')
+    const $chevronUp = document.querySelector('.chevron-up')
 
     $sortingDropdown?.addEventListener('click', dropdown.dropdownClickHandler)
     $sortingDropdown?.addEventListener('keydown', dropdown.dropdownKeydownHandler)
 
+    $chevronDown.addEventListener('click', dropdown.dropdownClickHandler)
+    $chevronDown.addEventListener('keydown', dropdown.dropdownKeydownHandler)
+
     document.addEventListener('click', dropdown.documentClickHandler)
     document.addEventListener('keydown', dropdown.documentKeydownHandler)
+    $chevronUp.addEventListener('click', dropdown.chevronUpClickHandler)
+    $chevronUp.addEventListener('keydown', dropdown.chevronUpKeydownHandler)
   },
 
   dropdownClickHandler (event) {
@@ -118,28 +140,45 @@ const dropdown = {
     }
   },
 
+  chevronUpClickHandler (event) {
+    event.preventDefault()
+    event.stopPropagation()
+    dropdown.toggleDropdown(false)
+  },
+
+  chevronUpKeydownHandler (event) {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault()
+      event.stopPropagation()
+      dropdown.toggleDropdown(false)
+    }
+  },
+
   toggleDropdown (isOpen) {
     const $dropdown = document.querySelector('.dropdown')
     const $sortingDropdown = document.querySelector('.sortingDropdown')
     const $sortMenu = document.getElementById('listbox')
-    const $chevronUp = document.querySelector('.chevron-up')
     const $chevronDown = document.querySelector('.chevron-down')
+    const $chevronUp = document.querySelector('.chevron-up')
 
     $dropdown.classList?.toggle('open', isOpen)
     $sortingDropdown?.setAttribute('aria-label', isOpen ? 'Menu déroulant ouvert. Vous pouvez trier les médias par plusieurs options.' : 'Menu déroulant fermé. Cliquez pour trier les médias par plusieurs options.')
     $sortingDropdown?.setAttribute('aria-expanded', isOpen.toString())
     $sortingDropdown?.setAttribute('aria-hidden', isOpen.toString())
-    $chevronDown?.setAttribute('aria-hidden', isOpen.toString())
+    $chevronDown?.setAttribute('aria-hidden', (!isOpen).toString())
     $sortMenu?.setAttribute('aria-expanded', isOpen.toString())
     $sortMenu?.setAttribute('aria-hidden', (!isOpen).toString())
     $chevronUp?.setAttribute('aria-hidden', (!isOpen).toString())
 
     if (isOpen === true) {
-      const firstButton = $sortMenu.querySelector('button')
-      if (firstButton) {
-        firstButton.focus()
-        console.log('firstButton', firstButton)
+      const $firstBouton = document.querySelector('.sort-entry')
+      const $chevronUp = document.querySelector('.chevron-up')
+      if ($firstBouton) {
+        $firstBouton.focus()
       }
+    } else {
+      const $chevronDown = document.querySelector('.chevron-down')
+      $chevronDown.focus()
     }
   },
 
